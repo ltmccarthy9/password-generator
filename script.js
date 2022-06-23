0bf55ddabaa6beef05b580ecee33fe1260a9a613
Liam McCarthy <ltmccarthy9@gmail.com>
2022-06-22 20:45:57 -0500

    starting algorithm

--- script.js
+++ script.js
@@ -3,13 +3,37 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-        var passwordLength = window.prompt("How many characters would you like? ")
-           var passwordLengthInt = parseInt(passwordLength)
-               if (passwordLengthInt < 8) {
-                 alert("Must be 8 or more characters long!")
-               } else if (passwordLengthInt > 128) {
-                 alert("Must be 128 or less characters long")
-               }
+    var mainList = []
+    var specialCharacters = [' ', '', '','','','','','','','','','','','','','',]
+    var lowerCase = []
+    var upperCase = []
+    var numberList = []
+
+    var phase1 = true;
+    while (phase1 == true) {
+    var passwordLength = window.prompt("How many characters would you like? ")
+    var passwordLengthInt = parseInt(passwordLength)
+            if (passwordLengthInt < 8) {
+                alert("Must be 8 or more characters long!")  
+            } else if (passwordLengthInt > 128) {
+                alert("Must be 128 or less characters long")
+            } else {
+            phase1 = false;
+            }
+        }
+    alert("your password will be " + passwordLengthInt + " characters long")
+        
+    var yesCount = 0
+    while (yesCount = 0) {
+        var specialYN = window.confirm("Do you want special characters in your password?")
+        if (specialYN) {
+        mainlist = mainlist.concat(specialCharacters)
+        yesCount = 1
+        } else {
+            var lowerYN = window.confirm("Do you want lowercase letters in your password?")
+        }
+    } 
+            
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -24,10 +48,4 @@ generateBtn.addEventListener("click", writePassword);
 
 
 // random function 
-
-var mainList = []
-var specialCharacters = [' ', '', '','','','','','','','','','','','','','',]
-var lowerCase = []
-var upperCase = []
-var numberList = []
 
